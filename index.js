a15c13669e3dab2c573302978b50496bfd379197
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-06-02 23:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Code your solution here
-function findMatching (list, name) {
-  return list.filter(function (driverName) {
+function findMatching (drivers, name) {
+  return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
 
-function fuzzyMatch (list, partialName) {
+function fuzzyMatch (drivers, partialName) {
   let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
+  return drivers.filter(function (driverName) {
     return driverName.slice(0, lengthOfName) === partialName;
   });
 }
 
-function matchName (list, name) {
-  return list.filter(function (driver) {
+function matchName (drivers, name) {
+  return drivers.filter(function (driver) {
     return driver.name.toLowerCase() === name.toLowerCase();
   });
 }
